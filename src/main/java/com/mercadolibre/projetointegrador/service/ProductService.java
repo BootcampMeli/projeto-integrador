6ad93779d76fd2e812b76492a1b03d50b1924d2b
Rayan Salewski <rayan.salewski@mercadolivre.com>
2021-07-06 01:17:16 -0300

    Renaming project

---
+++ src/main/java/com/mercadolibre/projetointegrador/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.mercadolibre.projetointegrador.service;
+
+import com.mercadolibre.projetointegrador.dtos.BatchDTO;
+import com.mercadolibre.projetointegrador.model.Product;
+
+public interface ProductService {
+    Product getProduct(BatchDTO item);
+}
